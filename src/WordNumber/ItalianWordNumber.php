0d714cd17a663f9915977ff67f829ffa3e30a8a5
Joao Gilberto Magalhães <joao@byjg.com.br>
2018-01-12 18:04:25 -0200

    Added Unit Test for ItalianWordNumber

--- src/WordNumber/ItalianWordNumber.php
+++ src/WordNumber/ItalianWordNumber.php
@@ -4,59 +4,89 @@ namespace ByJG\WordNumber;
 
 class ItalianWordNumber extends Base
 {
-	public function concatWord()
-	{
-		return "e";
-	}
+    public function concatWord()
+    {
+        return "e";
+    }
 
-	public function currency()
-	{
-		return array("euro", "euro", "centesimo", "centesimi");
-	}
+    public function currency()
+    {
+        return ["euro", "euro", "centesimo", "centesimi"];
+    }
 
-	public function unit()
-	{
-		return array(
-			"uno", "due", "tre", "quattro", "cinque", "sei", "sette", "otto", "nove"
-		);
-	}
+    public function unit()
+    {
+        return [
+            "uno",
+            "due",
+            "tre",
+            "quattro",
+            "cinque",
+            "sei",
+            "sette",
+            "otto",
+            "nove",
+        ];
+    }
 
-	public function elevenToNineteen()
-	{
-		return array(
-			"dieci", "undici", "dodici", "tredici", "quattordici", "quindici",
-			"sedici", "diciasette", "diciotto", "diciannove"
-		);
-	}
+    public function elevenToNineteen()
+    {
+        return [
+            "dieci",
+            "undici",
+            "dodici",
+            "tredici",
+            "quattordici",
+            "quindici",
+            "sedici",
+            "diciasette",
+            "diciotto",
+            "diciannove",
+        ];
+    }
 
-	public function twentyToNinety()
-	{
-		return array(
-			"venti", "trenta", "quaranta", "cinquanta", "sessanta", "settanta", "ottanta", "novanta"
-		);
-	}
+    public function twentyToNinety()
+    {
+        return [
+            "venti",
+            "trenta",
+            "quaranta",
+            "cinquanta",
+            "sessanta",
+            "settanta",
+            "ottanta",
+            "novanta",
+        ];
+    }
 
-	public function oneHundred()
-	{
-		return "cento";
-	}
+    public function oneHundred()
+    {
+        return "cento";
+    }
 
-	public function hundreds()
-	{
-		return array(
-			"mille", "duemila", "tremila", "quattromila", "cinquemila", "seimila",
-			"settemila", "ottomila", "novemila"
-		);
-	}
+    public function hundreds()
+    {
+        return [
+            "mille",
+            "duemila",
+            "tremila",
+            "quattromila",
+            "cinquemila",
+            "seimila",
+            "settemila",
+            "ottomila",
+            "novemila",
+        ];
+    }
 
-	public function tens()
-	{
-		return array(
-			array(" e ", "",""),
-			array(", ", "mille","mila"),
-			array(", ", "milione","milioni"),
-			array(", ", "miliardo","miliardi"),
-			array(", ", "bilione","bilioni")
-		);
-	}
+    public function tens()
+    {
+        return [
+            [" e ", "", ""],
+            [", ", "mille", "mila"],
+            [", ", "milione", "milioni"],
+            [", ", "miliardo", "miliardi"],
+            [", ", "bilione", "bilioni"],
+        ];
+    }
 }
